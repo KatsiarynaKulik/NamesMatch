a0329428e6137c4b582f22ac98c6aafc87cdc140
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-05-22 23:28:22 +0400

    Fixed: grammatical errors

--- NamesCompApp/ResultViewController.swift
+++ NamesCompApp/ResultViewController.swift
@@ -2,7 +2,8 @@
 //  ResultViewController.swift
 //  NamesCompApp
 //
-//  Created by Екатерина  on 22.02.23.
+//  Created by Katsiaryna Kulik  on 22.02.23.
+//  Copyright © 2023 Katsiaryna Kulik. All rights reserved.
 //
 
 import UIKit
